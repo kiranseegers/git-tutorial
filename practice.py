621fe52f54f70e6e7910dfd7ddca4e60bebeb60e
Kiran Seegers <kiranseegers@uchicago.edu>
2025-01-09 19:07:35 -0600

    Practice

---
+++ practice.py
@@ -0,0 +1,39 @@
+message = "Hello, world"
+print(message) 
+message = "hELLO, READER"
+print(message)
+message = "Hello, interpreter"
+print(message)
+a = 5
+b = -16
+print(a)
+print(b)
+c = 10
+type(c)
+c = 10.5 
+type(c) 
+message3 = """This message 
+spans  multiple 
+lines"""
+print(message3)
+message = 'Hello world, "I am gay"'
+print(message)
+x = 5
+y = 6
+print(f'the value of x is {x} and the value of y is {y}')
+print(f'The value of x*y is {x*y}')
+a = True 
+print(a)
+b = False
+print(b)
+a = 2 + 2
+b = 9
+print(a-b)
+a = 10/3
+print(a)
+print(type(a))
+a = 2 ** 3
+print(a)
+print(100<2)
+num_children = None
+print(num_children is None)
